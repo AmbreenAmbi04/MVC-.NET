213104be390f8c01ff17d4b13fc1c28a0e3897f4
Ambreen <ambreenambi04@gmail.com>
2025-10-13 11:38:17 +0500

    Made An Employee Controller!

---
+++ WebApplication1/Controllers/EmployeeController.cs
@@ -0,0 +1,34 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers;
+
+public class EmployeeController : Controller
+{
+    public IActionResult Index()
+    {
+        return Content("Welcome to the Employee Index Page.");
+    }
+    public IActionResult Details(int id)
+    {
+        string profile = string.Empty;
+        if (id == 1)
+        {
+            profile = "Ambreen";
+        }
+        else if (id == 2)
+        {
+            profile = "Ayesha";
+        }
+        else if (id == 3)
+        {
+            profile = "Zainab";
+        }
+        else
+        {
+            profile = "No record found";
+        }
+        return Content(profile);
+    }
+}
--- WebApplication1/obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45ad9dbc56caf6b91472f2396f9782bc1d8cc320")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7cd01f3e325059e2590d7cd3193e4d3f9fbd7c64")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
