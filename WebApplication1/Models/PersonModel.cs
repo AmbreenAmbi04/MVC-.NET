e1fe67bccc76a3e968c452c0af7fa79379d61573
Ambreen <ambreenambi04@gmail.com>
2025-10-14 11:21:30 +0500

    Testing Person Controller!

--- WebApplication1/Models/PersonModel.cs
+++ WebApplication1/Models/PersonModel.cs
@@ -6,8 +6,8 @@ public class PersonModel
 
     public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public string Address { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? Address { get; set; }
     public bool isPresent { get; set; }
 }
--- WebApplication1/obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97aacf8b33aef12cd43735104469f6d67d0e2632")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+034e92d14def1143f8278039a915da78a39326ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
