5404c5d196cd9d6e7dce4b2f9461c3b045d2d691
Ambreen <ambreenambi04@gmail.com>
2025-10-21 12:15:02 +0500

    Making A Form To Get Data!

--- WebApplication1/obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14ebe7093dc3efee61e960ba48c12893a4f4a6f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4408bf4619a19ae2bf4e65dfcfe55d4196a3e069")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
